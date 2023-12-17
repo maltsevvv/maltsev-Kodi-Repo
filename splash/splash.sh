ac26ba5a1a62d4104a56557516f6c658e5fdcf2c
Viachaslau <maltsev.v.v@hotmail.com>
2023-12-17 17:50:16 +0300

    Update splash.sh

--- splash/splash.sh
+++ splash/splash.sh
@@ -11,6 +11,10 @@ sed -i 's/console=tty1/console=tty3 consoleblank=0 loglevel=1 /' /boot/cmdline.t
 
 systemctl disable getty@tty1
 
+rm /opt/splash.png
+wget -P /opt https://raw.githubusercontent.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.png
+
+
 apt install -y fbi
 if ! grep -q "Description=Splash screen" /etc/systemd/system/splashscreen.service; then
   cat <<'EOF' > /etc/systemd/system/splashscreen.service
@@ -30,10 +34,6 @@ EOF
   systemctl enable splashscreen
 fi
 
-rm /opt/splash.png
-#wget -P /opt https://github.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.png
-wget -P /opt https://raw.githubusercontent.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.png
-
 if (systemctl -q is-active kodi.service); then
   systemctl stop kodi.service
   sleep 10
@@ -44,7 +44,5 @@ exit 1
 fi
 
 mv /usr/share/kodi/media/splash.jpg /usr/share/kodi/media/original_splash.jpg
-#wget -P /usr/share/kodi/media/ https://github.com/maltsevvv/maltsev-Kodi-Repo/blob/master/splash/splash.jpg
 wget -P /usr/share/kodi/media/ https://raw.githubusercontent.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.jpg
-sleep 10
 reboot
