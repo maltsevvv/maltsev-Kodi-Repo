464e805362d3f57587f7cae9893a8d53e97b7290
Viachaslau <maltsev.v.v@hotmail.com>
2023-12-27 18:29:51 +0300

    Add files via upload

--- splash/splash.sh
+++ splash/splash.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if ! grep -q "disable_splash=1" /boot/config.txt; then
-  cat <<'EOF' >> /boot/config.txt
 
-disable_splash=1
-EOF
-fi
+# if ! grep -q "disable_splash=1" /boot/config.txt; then
+  # cat <<'EOF' >> /boot/config.txt
+
+# disable_splash=1
+# EOF
+# fi
 
 sed -i 's/console=tty1/console=tty3 loglevel=3 /' /boot/cmdline.txt
 
@@ -32,18 +33,32 @@ elif (systemctl -q is-active kodi.service); then
 exit 1
 fi
 
-if [ ! -e /usr/share/kodi/media/original_splash.jpg ]; then
-  mv /usr/share/kodi/media/splash.jpg /usr/share/kodi/media/original_splash.jpg
-fi
+# if [ ! -e /usr/share/kodi/media/original_splash.jpg ]; then
+  # mv /usr/share/kodi/media/splash.jpg /usr/share/kodi/media/original_splash.jpg
+# fi
 
-if [ -e /home/pi/splash.jpg ]; then
-  echo "FIND YOU splash.jpg"
-  rm /usr/share/kodi/media/splash.jpg
-  cp /home/pi/splash.jpg /usr/share/kodi/media/
-else
-  echo "Download splash.jpg"
-  rm /usr/share/kodi/media/splash.jpg
-  wget -P /usr/share/kodi/media/ https://raw.githubusercontent.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.jpg
-fi
+# if [ -e /home/pi/splash.jpg ]; then
+  # echo "FIND YOU splash.jpg"
+  # rm /usr/share/kodi/media/splash.jpg
+  # cp /home/pi/splash.jpg /usr/share/kodi/media/
+# else
+  # echo "Download splash.jpg"
+  # rm /usr/share/kodi/media/splash.jpg
+  # wget -P /usr/share/kodi/media/ https://raw.githubusercontent.com/maltsevvv/maltsev-Kodi-Repo/master/splash/splash.jpg
+# fi
+
+##############################################
+#                SETTINGS KODI               #
+##############################################
+echo "---------------------------------------------------------"
+echo "PRESETTING KODI"
+echo "---------------------------------------------------------"
+cat <<'EOF' > /home/pi/.kodi/userdata/advancedsettings.xml
+	
+<advancedsettings version="1.0">
+    <splash>false</splash>
+</advancedsettings>
+EOF
+chown pi:pi /home/pi/.kodi/userdata/advancedsettings.xml
 
 reboot
